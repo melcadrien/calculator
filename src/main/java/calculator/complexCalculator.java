37565729575a6cfe52d8353f7d2dda395b7ed5ed
melcadrien <98002928+melcadrien@users.noreply.github.com>
2022-11-28 22:05:11 -0700

    Added calculating by order of operations method

--- src/main/java/calculator/complexCalculator.java
+++ src/main/java/calculator/complexCalculator.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.apache.commons.lang3.Validate;
@@ -7,7 +8,8 @@ import org.apache.commons.lang3.Validate;
 public class complexCalculator {
 
 	//private static final String FORMAT_REGULAR_EXPRESSION = "[0-9]*([\\+|\\-|\\/|\\*])[0-9]*";
-	private static final String FORMAT_REGULAR_EXPRESSION = "[0-9]*([\\+\\-\\/\\*]))*[0-9]*";
+//	private static final String FORMAT_REGULAR_EXPRESSION = "([0-9])*\\s*[\\+\\-\\/\\*]*\\s*([0-9])*";
+	private static final String FORMAT_REGULAR_EXPRESSION = "(([0-9])*\\s*[\\+\\-\\/\\*]\\s*)*([0-9])+";
 	//TODO: Order of operations: PEMDAS
 	//Rough idea is to go through the operator array, find either multiply or divide and run those.
 	//Then Add/subtract
@@ -15,6 +17,13 @@ public class complexCalculator {
 	//After that, floating point.
 	//First, wait for the junit from brother.  If it comes.
 
+	/*
+	 * In Steps:
+	 * # Start with separating addition and subtraction from multiplication and division.
+	 * # Add Parentheses search.[For exponents, ^ might be used or be ignored.]
+	 * # Add BigDecimal in future update.
+	 */
+	
 	public static enum Operator{
 		PLUS('+'), MINUS('-'), MULTIPLY('*'), DIVIDE('/');
 
@@ -40,7 +49,7 @@ public class complexCalculator {
 		userInput.close();
 	}
 
-	private static int calculate(String[] values, Operator[] operators) {
+	private static int calculateByInput(String[] values, Operator[] operators) {
 		int totalValue = Integer.parseInt(values[0]);
 		
 		for(int pos = 1; pos < values.length; pos++) {
@@ -61,17 +70,67 @@ public class complexCalculator {
 		}
 		return totalValue;
 	}
+	
+	private static int calculateByOperationOrder(String[] values, Operator[] operators) {
+		int totalValue = Integer.parseInt(values[0]);
+		
+		ArrayList<Integer> newValues = new ArrayList<Integer>();
+		ArrayList<Operator> newOperators = new ArrayList<Operator>();
+
+		//For Multiply and Divide
+		//Results are added into ArrayList to make it easier to add in values to be used for addition/subtraction
+		for(int pos = 1; pos < values.length; pos++) {
+			while(operators[pos-1] != Operator.PLUS && operators[pos-1] != Operator.MINUS) {
+				switch(operators[pos-1]){
+				case MULTIPLY:
+					totalValue *= Integer.parseInt(values[pos]);
+					break;
+				case DIVIDE:
+					totalValue /= Integer.parseInt(values[pos]);
+					break;
+				}
+				pos++;
+				if(pos == values.length)
+					break;
+			}
+			newValues.add(totalValue);
+			if(pos != values.length) {
+				newOperators.add(operators[pos-1]);
+				totalValue = Integer.parseInt(values[pos]);
+			}
+			if(pos == values.length-1 && (operators[pos-1] == Operator.PLUS || operators[pos-1] == Operator.MINUS)){
+				newValues.add(totalValue);
+			}
+		}
+		
+		//For Addition and Subtraction
+		if(newValues.size() != 1) {
+			totalValue = newValues.get(0);
+			for(int pos = 1; pos < newValues.size(); pos++) {
+				switch(newOperators.get(pos-1)){
+				case PLUS:
+					totalValue += newValues.get(pos);
+					break;
+				case MINUS:
+					totalValue -= newValues.get(pos);
+					break;
+				}
+			}
+		
+		}
+		return totalValue;
+	}
 
 	//Gets the values using the operators as a separator.
 	private static String[] createValues(String input) {
-		return input.split("\\D");
+		return input.split("\\s*\\D\\s*");
 	}
 
-	
+	//Notes: Broken, revise?
 	private static Operator[] createOperators(String input) {
 		//Creates an initial whitespace, which has to be removed.
 		//Maybe some sort of array that has a queue?
-		String[] initialOperators = input.split("\\d+");
+		String[] initialOperators = input.split("\\s*\\d+\\s*");
 		String[] OperatorString = new String[initialOperators.length-1];
 		System.arraycopy(initialOperators, 1, OperatorString, 0, OperatorString.length);
 
@@ -103,7 +162,7 @@ public class complexCalculator {
 		
 		Operator[] operators = createOperators(input);
 
-		return calculate(values,operators);
+		return calculateByOperationOrder(values,operators);
 	}
 
 
