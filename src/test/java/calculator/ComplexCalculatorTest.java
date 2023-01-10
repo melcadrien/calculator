0e6932c840a8c6cf56350c6b327e86c688dc702e
melcadrien <98002928+melcadrien@users.noreply.github.com>
2023-01-09 20:05:52 -0700

    Added more tests

--- src/test/java/calculator/ComplexCalculatorTest.java
+++ src/test/java/calculator/ComplexCalculatorTest.java
@@ -11,32 +11,67 @@ public class ComplexCalculatorTest {
     String input = "12|2";
     
     // Act
-    int result = complexCalculator.preCalculate(input);
+    int result = ComplexCalculator.preCalculate(input);
     
     // Assert
   }
-
-  @Test(expected = IllegalArgumentException.class)
-  public void preCalculate_WithSpaces_Fail() {
-    // Arrange
-    String input = "12 + 2";
-    
-    // Act
-    int result = complexCalculator.preCalculate(input);
-    
-    // Assert
-  }
-
+  
   @Test(expected = IllegalArgumentException.class)
   public void preCalculate_AlphanumericCharacter_Fail() {
     // Arrange
     String input = "12 + 2y";
     
     // Act
-    int result = complexCalculator.preCalculate(input);
+    int result = ComplexCalculator.preCalculate(input);
     
     // Assert
   }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void preCalculate_ReversedParentheses_Fail() {
+    // Arrange
+    String input = ")12 + 2(";
+    
+    // Act
+    int result = ComplexCalculator.preCalculate(input);
+    
+    // Assert
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void preCalculate_MissingParentheses_Fail() {
+    // Arrange
+    String input = "(12 + 2";
+    
+    // Act
+    int result = ComplexCalculator.preCalculate(input);
+    
+    // Assert
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void preCalculate_MissingOpenParentheses_Fail() {
+    // Arrange
+    String input = "12 + 2)";
+    
+    // Act
+    int result = ComplexCalculator.preCalculate(input);
+    
+    // Assert
+  }
+  
+
+  public void preCalculate_WithSpaces() {
+    // Arrange
+    String input = "12 + 2";
+    
+    // Act
+    int result = ComplexCalculator.preCalculate(input);
+    
+    // Assert
+    assertEquals(14, result);
+  }
+
 
   @Test
   public void preCalculate_Single_MultiplicationOperator() {
@@ -44,7 +79,7 @@ public class ComplexCalculatorTest {
     String input = "12*2";
     
     // Act
-    int result = complexCalculator.preCalculate(input);
+    int result = ComplexCalculator.preCalculate(input);
     
     // Assert
     assertEquals(24, result);
@@ -56,7 +91,7 @@ public class ComplexCalculatorTest {
     String input = "12*2*3";
     
     // Act
-    int result = complexCalculator.preCalculate(input);
+    int result = ComplexCalculator.preCalculate(input);
     
     // Assert
     assertEquals(72, result);
@@ -68,7 +103,7 @@ public class ComplexCalculatorTest {
     String input = "12*2+3";
     
     // Act
-    int result = complexCalculator.preCalculate(input);
+    int result = ComplexCalculator.preCalculate(input);
     
     // Assert
     assertEquals(27, result);
@@ -80,7 +115,7 @@ public class ComplexCalculatorTest {
     String input = "3+2*12";
     
     // Act
-    int result = complexCalculator.preCalculate(input);
+    int result = ComplexCalculator.preCalculate(input);
     
     // Assert
     assertEquals(27, result);
@@ -92,7 +127,7 @@ public class ComplexCalculatorTest {
     String input = "(3+2)*12";
     
     // Act
-    int result = complexCalculator.preCalculate(input);
+    int result = ComplexCalculator.preCalculate(input);
     
     // Assert
     assertEquals(60, result);
@@ -104,10 +139,58 @@ public class ComplexCalculatorTest {
     String input = "((3+2)*12)";
     
     // Act
-    int result = complexCalculator.preCalculate(input);
+    int result = ComplexCalculator.preCalculate(input);
     
     // Assert
     assertEquals(60, result);
   }
+  
+  @Test
+  public void preCalculate_Multiple_MixedOperatorsWithMultipleParentheses_ReverseParentheses() {
+    // Arrange
+    String input = "(3+(2*12))";
+    
+    // Act
+    int result = ComplexCalculator.preCalculate(input);
+    
+    // Assert
+    assertEquals(27, result);
+  }
+  
+  @Test
+  public void preCalculate_Multiple_MixedOperatorsWithMultipleParentheses_DoubleParentheses() {
+	    // Arrange
+	    String input = "((3+2*12))";
+	    
+	    // Act
+	    int result = ComplexCalculator.preCalculate(input);
+	    
+	    // Assert
+	    assertEquals(27, result);
+	  }
+  
+  @Test
+  public void preCalculate_Multiple_MixedOperatorsWithMultipleParentheses_TwoSeparateParentheses() {
+	    // Arrange
+	    String input = "(3+2)+(4*3)";
+	    
+	    // Act
+	    int result = ComplexCalculator.preCalculate(input);
+	    
+	    // Assert
+	    assertEquals(17, result);
+	  }
+  
+  @Test
+  public void preCalculate_Multiple_MixedOperatorsWithMultipleParentheses_NestedParentheses() {
+	    // Arrange
+	    String input = "((3+2)+(4*3)+4)";
+	    
+	    // Act
+	    int result = ComplexCalculator.preCalculate(input);
+	    
+	    // Assert
+	    assertEquals(21, result);
+	  }
   
 }
